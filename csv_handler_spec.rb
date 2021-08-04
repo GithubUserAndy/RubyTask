ebfc26553ada5c28724ffb5a8269ee1120796d1f
Harry Knee <harryknee@Harrys-MacBook-Pro.local>
2021-08-04 15:41:28 +0100

    Formats csv file in to 2d array, capitalizes names, removes rows with optout as true. Tests added for each.

--- csv_handler_spec.rb
+++ csv_handler_spec.rb
@@ -3,23 +3,33 @@ require './csv_handler.rb'
 
 #describing behaviour of CsvHandler
 describe CsvHandler do
-	subject { CsvHandler.new file_name }
-  let(:file_name) { 'data.csv' }
+	subject { CsvHandler.new('data.csv') }
 
-  describe '#loaded?' do
-    it 'is true when file is loaded' do
-      expect(subject.loaded?).to eq true
-    end
-    
-    context 'when file is invalid' do
-      let(:file_name) {'data_invalid.csv'}
+  describe '#format' do
+    it 'returns array template' do
 
-      it 'is false when invalid file is loaded' do
-        expect(subject.loaded?).to eq false
-      end      
+      expect(subject.format).to eq([["derek trotter","sa1 1nw", "no"],["Rodney trotter","Sa6 W23","yes"],["Rick Grimes","Sa2 6tr","no"]])    
     end
   end
 
+  describe '#capitalise_names' do
+    it 'returns data array with names capitalised' do
+      expect(subject.capitalise_names).to eq([["Derek Trotter","sa1 1nw", "no"],["Rodney Trotter","Sa6 W23","yes"],["Rick Grimes","Sa2 6tr","no"]])
+    end
+  end
 
+  describe '#uppercase_postcodes' do
+    it 'returns data array with capitalised postcodes and names' do
+      expect(subject.uppercase_postcodes).to eq([["Derek Trotter","SA1 1NW", "no"],["Rodney Trotter","SA6 W23","yes"],["Rick Grimes","SA2 6TR","no"]])
+    end    
+  end
+
+  describe '#remove_if_optout' do
+    it 'returns data array with rows with optout set to yes removed and capitalised postcodes and names' do 
+      expect(subject.remove_if_optout).to eq([["Derek Trotter","SA1 1NW", "no"],["Rick Grimes","SA2 6TR","no"]])
+    end
+  end
 end
+
+
 
