2be2b6af614e2f8fe49e011dbbc4ae14931eb54f
Harry Knee <harryknee@Harrys-MacBook-Pro.local>
2021-08-05 15:16:49 +0100

    Removed iteration over the same object multiple times, generally tidied up code.

--- csv_handler.rb
+++ csv_handler.rb
@@ -2,10 +2,11 @@ require 'csv'
 
 class CsvHandler 
   # initialize class
+
+  @loaded = false
 	def initialize(file_name)
-		@loaded = false
 		begin # try to read file
-      @csv_data = CSV.read(file_name)
+      @csv_data = CSV.parse(File.read(file_name))
   	rescue Errno::ENOENT # closes program gracefully if error
     	print "File not found"
       exit 1
@@ -13,46 +14,34 @@ class CsvHandler
     @loaded = true
 	end
 
-  # getter for loaded
   def loaded?
     @loaded
   end
 
-  # 1) format the csv file in to 2d array (no titles)
   def format
+  # 1) format the csv file in to 2d array (no titles)
     output = []
-    @csv_data.drop(1).each do |i|
-      output.push(i)
-    end
-
+    @csv_data.map { |row| output.push(row)}
+    output.shift
   
-  # 2) capitalise the names
-    
+  # 2) remove rows with opt out set to yes
     output.each do |i|
-      name = ''
-      split_name = i[0].split()
-      split_name.each do |j|
-        name += ' ' + j.capitalize
-      end
-      i[0] = name[1..-1] 
+    if i[2] == 'yes'
+      output.delete(i)
     end
 
+  # 3) capitalise the names  
+    output.each do |i|
+      i[0] = i[0].split.map{ |part| part.capitalize}.join(' ')
+    end
 
-
-  # 3) set postcodes to uppercase
-
+  # 4) set postcodes to uppercase
     output.each do |i|
       i[1] = i[1].upcase
     end
 
 
 
-  # 4) remove rows where opt out = yes
-
-    output.each do |i|
-      if i[2] == 'yes'
-        output.delete(i)
-      end
     end
 
     output
--- csv_handler_spec.rb
+++ csv_handler_spec.rb
@@ -5,10 +5,26 @@ require './csv_handler.rb'
 describe CsvHandler do
 	subject { CsvHandler.new(file_name) }
   let(:file_name) { 'data.csv' }
+=begin
+  describe '#loaded?' do
+    it 'is true when file is loaded' do
+      expect(subject.loaded?).to eq true
+    end
 
+    context 'when file is invalid' do
+      let(:file_name) {'data_invalid.csv'}
+
+      it 'is false when invalid file is loaded' do
+        expect(subject.loaded?).to eq false
+      end      
+    end
+  end
+=end
   describe '#format' do
-    it 'returns contents of csv file in 2d array format' do
-      expect(subject.format).to eq([["Derek Trotter","SA1 1NW", "no"],["Rick Grimes","SA2 6TR","no"]])    
+    it 'formats the csv correctly' do
+      expect(subject.format).to eq(
+        [["Derek Trotter","SA1 1NW", "no"],
+        ["Rick Grimes","SA2 6TR","no"]])    
     end
   end
 
