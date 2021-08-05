4b712d5fc7468f7f7d75c4c32ed521c5971cf666
Harry Knee <harryknee@Harrys-MacBook-Pro.local>
2021-08-05 10:44:33 +0100

    Changed for loops to iterators and condensed functions in to the format function

--- csv_handler.rb
+++ csv_handler.rb
@@ -4,15 +4,12 @@ class CsvHandler
   # initialize class
 	def initialize(file_name)
 		@loaded = false
-    
-    
 		begin # try to read file
       @csv_data = CSV.read(file_name)
   	rescue Errno::ENOENT # closes program gracefully if error
     	print "File not found"
       exit 1
     end
- 
     @loaded = true
 	end
 
@@ -21,53 +18,44 @@ class CsvHandler
     @loaded
   end
 
-  # formatting csv file in to 2d array (no titles)
+  # 1) format the csv file in to 2d array (no titles)
   def format
-    output = [@csv_data.length()-1]
-    for i in 1...@csv_data.length()
-      output[i-1] = @csv_data[i]
-    end
-    output
-  end
-  
-
-  # 1) capitalise the names 
-  def capitalise_names
-    output = format
-    for i in 0...format.length()
-      # assuming here that we know the name will be in the first column- maybe add checker later
-      # splitting a name and capitalising the first letter of each part of the name
-      split_name = format[i][0].split
-      name = ''
-      for j in 0...split_name.length()
-        if j != split_name.length()-1
-          name += split_name[j].capitalize + ' '
-        else
-          name += split_name[j].capitalize
-        end
-      end
-      output[i][0] = name
-    end
-    output
-  end 
-
-  # 2) set postcodes to uppercase
-  def uppercase_postcodes
-    output = capitalise_names
-    for i in 0...capitalise_names.length()
-      output[i][1] = capitalise_names[i][1].upcase
-    end
-    output
-  end
-
-  # 3) remove rows where opt out = yes
-  def remove_if_optout
     output = []
-    for i in 0...uppercase_postcodes.length()
-      if uppercase_postcodes[i][2] != 'yes'
-        output.push(uppercase_postcodes[i])
+    @csv_data.drop(1).each do |i|
+      output.push(i)
+    end
+
+  
+  # 2) capitalise the names
+    
+    output.each do |i|
+      name = ''
+      split_name = i[0].split()
+      split_name.each do |j|
+        name += ' ' + j.capitalize
+      end
+      i[0] = name[1..-1] 
+    end
+
+
+
+  # 3) set postcodes to uppercase
+
+    output.each do |i|
+      i[1] = i[1].upcase
+    end
+
+
+
+  # 4) remove rows where opt out = yes
+
+    output.each do |i|
+      if i[2] == 'yes'
+        output.delete(i)
       end
     end
+
     output
   end
+
 end
--- csv_handler_spec.rb
+++ csv_handler_spec.rb
@@ -3,32 +3,16 @@ require './csv_handler.rb'
 
 #describing behaviour of CsvHandler
 describe CsvHandler do
-	subject { CsvHandler.new('data.csv') }
+	subject { CsvHandler.new(file_name) }
+  let(:file_name) { 'data.csv' }
 
   describe '#format' do
-    it 'returns array template' do
-
-      expect(subject.format).to eq([["derek trotter","sa1 1nw", "no"],["Rodney trotter","Sa6 W23","yes"],["Rick Grimes","Sa2 6tr","no"]])    
+    it 'returns contents of csv file in 2d array format' do
+      expect(subject.format).to eq([["Derek Trotter","SA1 1NW", "no"],["Rick Grimes","SA2 6TR","no"]])    
     end
   end
 
-  describe '#capitalise_names' do
-    it 'returns data array with names capitalised' do
-      expect(subject.capitalise_names).to eq([["Derek Trotter","sa1 1nw", "no"],["Rodney Trotter","Sa6 W23","yes"],["Rick Grimes","Sa2 6tr","no"]])
-    end
-  end
 
-  describe '#uppercase_postcodes' do
-    it 'returns data array with capitalised postcodes and names' do
-      expect(subject.uppercase_postcodes).to eq([["Derek Trotter","SA1 1NW", "no"],["Rodney Trotter","SA6 W23","yes"],["Rick Grimes","SA2 6TR","no"]])
-    end    
-  end
-
-  describe '#remove_if_optout' do
-    it 'returns data array with rows with optout set to yes removed and capitalised postcodes and names' do 
-      expect(subject.remove_if_optout).to eq([["Derek Trotter","SA1 1NW", "no"],["Rick Grimes","SA2 6TR","no"]])
-    end
-  end
 end
 
 
