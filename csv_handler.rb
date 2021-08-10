705f8faeeaf5d7991fd65a840e3b280068f1db14
Harry Knee <harryknee@Harrys-MacBook-Pro.local>
2021-08-10 12:13:50 +0100

    Applied the principal of DRY by wrapping csv_data in a method

--- csv_handler.rb
+++ csv_handler.rb
@@ -1,7 +1,6 @@
 require 'csv'
 
 class CsvHandler 
-  @loaded = false
 	def initialize(file_name)
 		begin 
       @csv_data = CSV.read(file_name)
@@ -9,38 +8,30 @@ class CsvHandler
     	print "File not found"
       exit 1
     end
-    @loaded = true
 	end
 
+  def csv_data
+    @csv_data.select { |i| i[2] != 'yes'}[1..-1]
+  end
 
   def hash_format
-    @csv_data.each do |i|
-      if i[2] == 'yes'
-        @csv_data.delete(i)
-      end
-    end
-    @csv_data.map { |i|
+    csv_data.map { |i|
       row = Hash.new
       {
         :name => i[0].split.map{ |part| part.capitalize}.join(' '),
         :postcode => i[1].upcase,
         :opt_out => i[2]
       }
-    }[1..-1]
+    }
   end
   
   def arr_format
-    @csv_data.each do |i|
-      if i[2] == 'yes'
-        @csv_data.delete(i)
-      end
-    end
-    @csv_data.map { |i|
+    csv_data.map { |i|
       [
         i[0].split.map{ |part| part.capitalize}.join(' '),
         i[1].upcase,
         i[2]
       ]
-    }[1..-1]
+    }
   end
 end
