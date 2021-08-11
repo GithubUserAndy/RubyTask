a0c71bdbad4f96a23bb0169a776784708d8bb6c8
Matthew <matthew.a423@gmail.com>
2021-08-11 15:31:44 +0100

    Refactor formats composition
    * Use csv headers to improve readability

--- csv_handler.rb
+++ csv_handler.rb
@@ -2,40 +2,36 @@ require 'csv'
 
 class CsvHandler 
 	def initialize(file_name)
-		begin 
-      @csv_data = CSV.read(file_name)
-  	rescue Errno::ENOENT 
+		begin
+      @csv_data = CSV.read(file_name, headers: true)
+  	rescue Errno::ENOENT
     	print "File not found"
       exit 1
     end
 	end
 
   def csv_data
-    @csv_data[1..-1]
+    @csv_data
   end
 
   def hash_format
-    csv_data.map do |row|
-      next if row[2] == 'yes'
+    base_data
+  end
 
-      {
-        name: capitalize_each_word(row[0]),
-        postcode: row[1].upcase,
-        opt_out: row[2]
-      }
-    end.compact
+  def base_data
+    csv_data.map do |row|
+      next if row["Opt out"] == 'yes'
+
+      [
+        [:name, capitalize_each_word(row["Name"])],
+        [:postcode, row["Postcode"].upcase],
+        [:opt_out, row["Opt out"]]
+      ]
+    end.compact.map(&:to_h)
   end
 
   def arr_format
-    csv_data.map do |row|
-      next if row[2] == 'yes'
-
-      [
-        capitalize_each_word(row[0]),
-        row[1].upcase,
-        row[2]
-      ]
-    end.compact
+    base_data.map(&:values)
   end
 
   private
--- csv_handler_spec.rb
+++ csv_handler_spec.rb
@@ -7,27 +7,35 @@ describe CsvHandler do
 
   describe '#hash_format' do
     it 'formats the csv correctly' do
-      expect(subject.hash_format).to eq([{
-          :name=>"Derek Trotter",
-          :opt_out=>"no",
-          :postcode=>"SA1 1NW"
-        },{
+      expect(subject.hash_format).to eq(
+        [
+          {
+            :name=>"Derek Trotter",
+            :opt_out=>"no",
+            :postcode=>"SA1 1NW"
+          },
+          {
           :name=>"Rick Grimes",
           :opt_out=>"no",
-          :postcode=>"SA2 6TR"}])    
+          :postcode=>"SA2 6TR"}
+        ]
+      )
     end
   end
   describe '#arr_format' do
     it 'formats the csv correctly' do
-      expect(subject.arr_format).to eq([[
-        "Derek Trotter",
-        "SA1 1NW",
-        "no"
-        ],[
-        "Rick Grimes",
-        "SA2 6TR",
-        "no"
-      ]])    
+      expect(subject.arr_format).to eq([
+        [
+          "Derek Trotter",
+          "SA1 1NW",
+          "no"
+        ],
+        [
+          "Rick Grimes",
+          "SA2 6TR",
+          "no"
+        ]
+      ])
     end
   end
 end
